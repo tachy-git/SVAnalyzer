addc51f073cbb77340b7451d109fdea2c2b70f37
Taehee Kim <eikz.il2ljlpl@gmail.com>
2026-02-09 15:47:07 +0900

    first try for vertex fitting

--- plugins/staMuon.cc
+++ plugins/staMuon.cc
@@ -20,6 +20,12 @@
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 
+#include "RecoVertex/KalmanVertexFit/interface/KalmanVertexFitter.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/Utilities/interface/ESInputTag.h"
+
 #include "TTree.h"
 #include "TLorentzVector.h"
 #include "TVector3.h"
@@ -43,6 +49,7 @@ private:
   // Tokens
   edm::EDGetTokenT<std::vector<pat::Muon>> muonToken_;
   edm::EDGetTokenT<std::vector<pat::Muon>> disMuonToken_;
+  edm::ESGetToken<TransientTrackBuilder, TransientTrackRecord> ttBuilderToken_;
 
   // Tree
   TTree* tree_;
@@ -60,11 +67,20 @@ private:
   std::vector<float> outer_innerEta_;
   std::vector<float> outer_innerPhi_;
 
+  std::vector<float> dr_;
+  std::vector<float> ptRatio_;
+
   // Standalone-pair info (within each collection separately)
   std::vector<float> sa_mll_;
+  std::vector<float> sa_mll2_;
   std::vector<float> sa_mu1_pt_;
   std::vector<float> sa_mu2_pt_;
+  std::vector<float> sa_mu1_ip_;
+  std::vector<float> sa_mu2_ip_;
   std::vector<int>   sa_coll_;            // 0=slimmedMuons, 1=slimmedDisplacedMuons
+
+  std::vector<float> sa_Lxy_;
+  std::vector<float> sa_prob_;
 
   // Helpers
   static bool passSASelection(const pat::Muon& mu) {
@@ -87,6 +103,9 @@ staMuon::staMuon(const edm::ParameterSet& iConfig) {
 
   muonToken_     = consumes<std::vector<pat::Muon>>( iConfig.getParameter<edm::InputTag>("muons") );
   disMuonToken_     = consumes<std::vector<pat::Muon>>( iConfig.getParameter<edm::InputTag>("disMuons") );
+  ttBuilderToken_ =
+      esConsumes<TransientTrackBuilder, TransientTrackRecord>(
+          edm::ESInputTag("", "TransientTrackBuilder"));
 }
 
 
@@ -107,11 +126,19 @@ void staMuon::beginJob() {
   tree_->Branch("outer_innerEta", &outer_innerEta_);
   tree_->Branch("outer_innerPhi", &outer_innerPhi_);
 
+  tree_->Branch("dr", &dr_);
+  tree_->Branch("ptRatio", &ptRatio_);
+
   tree_->Branch("sa_coll",   &sa_coll_);
   tree_->Branch("sa_mll",    &sa_mll_);
+  tree_->Branch("sa_mll2_",    &sa_mll2_);
   tree_->Branch("sa_mu1_pt", &sa_mu1_pt_);
   tree_->Branch("sa_mu2_pt", &sa_mu2_pt_);
+  tree_->Branch("sa_mu1_ip", &sa_mu1_ip_);
+  tree_->Branch("sa_mu2_ip", &sa_mu2_ip_);
 
+  tree_->Branch("sa_Lxy",    &sa_Lxy_);
+  tree_->Branch("sa_prob",    &sa_prob_);
 }
 
 
@@ -129,14 +156,23 @@ void staMuon::clearVectors() {
   outer_innerEta_.clear();
   outer_innerPhi_.clear();
 
+  dr_.clear();
+  ptRatio_.clear();
+
   sa_coll_.clear();
   sa_mll_.clear();
+  sa_mll2_.clear();
   sa_mu1_pt_.clear();
   sa_mu2_pt_.clear();
+  sa_mu1_ip_.clear();
+  sa_mu2_ip_.clear();
+
+  sa_Lxy_.clear();
+  sa_prob_.clear();
 }
 
 // -------------------- analyze --------------------
-void staMuon::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
+void staMuon::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
   clearVectors();
 
@@ -146,6 +182,10 @@ void staMuon::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
   iEvent.getByToken(muonToken_, muons);
   iEvent.getByToken(disMuonToken_, disMuons);
 
+  //edm::ESHandle<TransientTrackBuilder> ttBuilder;
+  //iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder", ttBuilder);
+  const auto& ttBuilder = iSetup.getData(ttBuilderToken_);
+
   // Helper lambda to process a collection
   auto processMuonCollection = [&](const std::vector<pat::Muon>& coll, int collId) {
     // 1) Check the global muons
@@ -154,26 +194,33 @@ void staMuon::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
       mu_coll_.push_back(collId);
       mu_charge_.push_back(mu.charge());
 
+
       float i_pt = -1.f, i_eta = -99.f, i_phi = -99.f;
       float o_pt = -1.f, o_eta = -99.f, o_phi = -99.f;
 
       // inner track
       if (mu.innerTrack().isNonnull()) {
         const reco::Track* intrk = mu.innerTrack().get();
-        if( intrk->outerOk() ){
+        /*if( intrk->outerOk() ){
           i_pt  = intrk->outerMomentum().rho();
           i_eta = intrk->outerPosition().eta();
           i_phi = intrk->outerPosition().phi();
-        }
+        }*/
+        i_pt = intrk->pt();
+        i_eta = intrk->eta();
+        i_phi = intrk->phi();
       }
       // outer track
       if (mu.outerTrack().isNonnull()) {
         const reco::Track* outtrk = mu.outerTrack().get();
-        if( outtrk->innerOk() ){
+        /*if( outtrk->innerOk() ){
           o_pt  = outtrk->innerMomentum().rho();
           o_eta = outtrk->innerPosition().eta();
           o_phi = outtrk->innerPosition().phi();
-        }
+        }*/
+        o_pt = outtrk->pt();
+        o_eta = outtrk->eta();
+        o_phi = outtrk->phi();
       }
 
       inner_outerPt_.push_back(i_pt);
@@ -182,6 +229,15 @@ void staMuon::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
       outer_innerPt_.push_back(o_pt);
       outer_innerEta_.push_back(o_eta);
       outer_innerPhi_.push_back(o_phi);
+
+      float deta = i_eta - o_eta;
+      float dphi = TVector2::Phi_mpi_pi(i_phi-o_phi);
+      double dr = std::sqrt(deta*deta + dphi*dphi);
+      dr_.push_back(dr);
+
+      if( i_pt<0 || o_pt<0 ) continue;
+      double ptRatio = ( i_pt - o_pt ) / i_pt ;
+      ptRatio_.push_back(ptRatio);
 
     }
 
@@ -210,9 +266,35 @@ void staMuon::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
         // Save the two muon pT (ordered)
         float pt1 = sa[i]->pt();
         float pt2 = sa[j]->pt();
+        float ip1 = sa[i]->dB();
+        float ip2 = sa[j]->dB();
         if (pt2 > pt1) std::swap(pt1, pt2);
         sa_mu1_pt_.push_back(pt1);
         sa_mu2_pt_.push_back(pt2);
+        sa_mu1_ip_.push_back(ip1);
+        sa_mu2_ip_.push_back(ip2);
+
+        if( ip1 < 1. || ip2 < 1.) continue;
+        sa_mll2_.push_back(mll);
+
+        // Kalman Vertex Fitter
+        KalmanVertexFitter kvf(false);
+        std::vector<reco::TransientTrack> tTracks;
+
+        tTracks.emplace_back(ttBuilder.build( sa[i]->bestTrack() ));
+        tTracks.emplace_back(ttBuilder.build( sa[j]->bestTrack() ));
+
+        TransientVertex tv = kvf.vertex(tTracks);
+        if (!tv.isValid()) continue;
+
+        //GlobalPoint v = tv.position();
+        auto v = tv.position();
+        float Lxy = std::hypot(v.x(), v.y());
+        float prob = TMath::Prob(tv.totalChiSquared(), tv.degreesOfFreedom());
+
+        sa_Lxy_.push_back(Lxy);
+        sa_prob_.push_back(prob);
+
       }
     }
   };
